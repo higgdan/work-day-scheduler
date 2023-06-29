8ffc6758b2403ca078d627375d75f42cb3d242d8
danhigg <higgins.daniel@gmail.com>
2023-06-29 22:12:18 +1000

    added text block function

--- script.js
+++ script.js
@@ -21,3 +21,47 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+// define the set of all time blocks to be created, in 24hr format
+let hours9to5 = ["09","10","11","12","13","14","15","16","17"]
+
+// define global scope for the container that populate-time-blocks function and event listener will refer to
+scheduleEl = $("#schedule");
+
+// function for constructing each time block,
+// generate elements and add attributes, classes to each (as given from example in intial HTML file)
+let populateTimeBlocks = function() {
+  // for loop to create time block for each hour in hours9to5 array
+  for (let hour of hours9to5) {
+    // define local references to elements
+    let hourContainerEl = $("<div>");
+    let hourTitleEl = $("<div>");
+    let textAreaEl = $("<textarea>");
+    let saveButtonEl = $("<button>");
+    let iconEl = $("<i>");
+
+    // set unique id using the value from the hours9to5 array
+    hourContainerEl.attr("id", "hour-" + hour);
+    hourContainerEl.addClass("row time-block past");
+    hourTitleEl.addClass("col-2 col-md-1 hour text-center py-3");
+    hourTitleEl.text(hour);
+    textAreaEl.addClass("col-8 col-md-10 description");
+    textAreaEl.attr("rows", "3");
+    saveButtonEl.addClass("btn saveBtn col-2 col-md-1");
+    saveButtonEl.attr("aria-label", "save");
+    iconEl.addClass("fas fa-save");
+    iconEl.attr("aria-hidden", "true");
+
+    // append populated elements to the time block
+    hourContainerEl.append(hourTitleEl);
+    hourContainerEl.append(textAreaEl);
+    saveButtonEl.append(iconEl);
+    hourContainerEl.append(saveButtonEl);
+
+    // append finished time block onto the end of the schedule container
+    scheduleEl.append(hourContainerEl);
+  }
+}
+
+// execute funtion to populate page with content
+populateTimeBlocks();
